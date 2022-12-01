479226538f2a459b4203f7be18fffc56a5edf826
ashish-yadav07 <ashish.yadav@sunfox.in>
2022-12-01 16:11:16 +0530

    remove master key from the initialize() in BeatoApplication class.

--- app/src/main/java/com/example/beatopoc/BeatoApplication.kt
+++ app/src/main/java/com/example/beatopoc/BeatoApplication.kt
@@ -4,10 +4,6 @@ package com.example.beatopoc
 import `in`.sunfox.healthcare.commons.android.spandan_sdk.OnInitializationCompleteListener
 import `in`.sunfox.healthcare.commons.android.spandan_sdk.SpandanSDK
 import android.app.Application
-import android.util.Log
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class BeatoApplication:Application() {
 
@@ -18,7 +14,7 @@ class BeatoApplication:Application() {
      * sdk initialization*/
     override fun onCreate() {
         super.onCreate()
-        SpandanSDK.initialize(this@BeatoApplication,"4u838u43u439u3",object : OnInitializationCompleteListener{
+        SpandanSDK.initialize(this@BeatoApplication,"enter master api key",object : OnInitializationCompleteListener{
             override fun onInitializationSuccess(p0: String) {
                 token = p0
             }
