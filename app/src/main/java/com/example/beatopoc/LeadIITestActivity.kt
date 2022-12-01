19824900f5820f19aab8d132b46a33069abf757f
ashish yadav <118358675+ashish-yadav07@users.noreply.github.com>
2022-12-01 15:03:14 +0530

    Bug fix depdency structure (#2)
    * dependency implementation and refactoring the code.
    
    * refactoring name
    
    * local properties name changed to repo prefix
    
    * refine gradle
    
    * initialization in application clss
    
    * implement application class
    
    * BeatoApplication renamed.
    
    * regenerate token api called if validation failed.
    
    * implement spandan sdk version 24
    
    * token not initialized error resolved by try catch
    
    * send device id in refresh token api.

--- app/src/main/java/com/example/beatopoc/LeadIITestActivity.kt
+++ app/src/main/java/com/example/beatopoc/LeadIITestActivity.kt
@@ -27,107 +27,121 @@ class LeadIITestActivity : AppCompatActivity() {
     private lateinit var ecgPosition: EcgPosition
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_lead_iitest)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_lead_iitest)
 
         binding.activityMainTextviewCurrentPosition.text = "Please select the lead"
 
-        spandanSDK = SpandanSDK.getInstance()
+        try {
+            spandanSDK = SpandanSDK.getInstance()
 
-        /**
-         * step :-2
-         * set callback for device connectivity.*/
-        spandanSDK.setOnDeviceConnectionStateChangedListener {
-            when(it){
-                DeviceConnectionState.DISCONNECTED -> { binding.activityMainLayoutDeviceConnectionStatus.setBackgroundColor(Color.RED) }
-                DeviceConnectionState.CONNECTED -> {  }
-                DeviceConnectionState.VERIFIED -> { binding.activityMainLayoutDeviceConnectionStatus.setBackgroundColor(Color.GREEN) }
-                DeviceConnectionState.VERIFICATION_TIME_OUT -> {}
-                DeviceConnectionState.USB_CONNECTION_PERMISSION_DENIED -> {}
-            }
-        }
-        /**
-         * step :-3
-         * create ecg test..*/
-        ecgTest = spandanSDK.createTest(EcgTestType.LEAD_TWO,object : EcgTestCallback {
-            override fun onTestFailed(p0: Int) {
-
-            }
-
-            override fun onTestStarted(p0: EcgPosition) {
-                Toast.makeText(this@LeadIITestActivity,"test started",Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onElapsedTimeChanged(p0: Long, p1: Long) {
-                binding.activityMainProgressbarTestStatus.progress = p0.toInt()
-                binding.progressBar8.progress = p0.toInt()
-            }
-
-            override fun onReceivedData(p0: String) {
-
-            }
-
-            override fun onPositionRecordingComplete(
-                p0: EcgPosition,
-                p1: java.util.ArrayList<Double>?,
-            ) {
-                if(p1!=null)
-                    ecgPoints[p0] = p1
-            }
-
-        })
-
-
-        binding.activityMainLayoutDeviceConnectionStatus.setBackgroundColor(if(UsbConnectionHelper.INSTANCE.isDeviceConnected) Color.GREEN else Color.RED)
-
-
-        binding.progressBar8.setOnClickListener {
-            ecgPosition = EcgPosition.LEAD_2
-            binding.activityMainTextviewCurrentPosition.text = EcgPosition.LEAD_2.name
-        }
-
-        /***
-         * step :-4
-         * start ecg test.*/
-        binding.activityMainBtnStartTest.setOnClickListener {
-            if(!::ecgPosition.isInitialized)
-                Toast.makeText(this,"please select any lead", Toast.LENGTH_SHORT).show()
-            else if(!UsbConnectionHelper.INSTANCE.isDeviceConnected)
-                Toast.makeText(this,"Please connect the device first.", Toast.LENGTH_SHORT).show()
-            else
-                ecgTest.start(ecgPosition)
-        }
-
-
-
-        /**
-         * step :-5
-         * generate ecg report*/
-        binding.activityMainBtnGenerateReport.setOnClickListener {
-            spandanSDK.generateReport(12,ecgPoints,object : OnReportGenerationStateListener {
-                override fun onReportGenerationSuccess(p0: EcgReport) {
-                    ecgReport = p0
-                    runOnUiThread {
-                        Toast.makeText(this@LeadIITestActivity,"report generated",Toast.LENGTH_SHORT).show()
+            /**
+             * step :-2
+             * set callback for device connectivity.*/
+            spandanSDK.setOnDeviceConnectionStateChangedListener {
+                when (it) {
+                    DeviceConnectionState.DISCONNECTED -> {
+                        binding.activityMainLayoutDeviceConnectionStatus.setBackgroundColor(Color.RED)
                     }
+                    DeviceConnectionState.CONNECTED -> {}
+                    DeviceConnectionState.VERIFIED -> {
+                        binding.activityMainLayoutDeviceConnectionStatus.setBackgroundColor(Color.GREEN)
+                    }
+                    DeviceConnectionState.VERIFICATION_TIME_OUT -> {}
+                    DeviceConnectionState.USB_CONNECTION_PERMISSION_DENIED -> {}
+                }
+            }
+            /**
+             * step :-3
+             * create ecg test..*/
+            ecgTest = spandanSDK.createTest(EcgTestType.LEAD_TWO, object : EcgTestCallback {
+                override fun onTestFailed(p0: Int) {
+
                 }
 
-                override fun onReportGenerationFailed(p0: Int, p1: String) {
-                    Log.d("SdkImpl.TAG", "onReportGenerationFailed: $p1")
-                    runOnUiThread {
-                        Toast.makeText(this@LeadIITestActivity,"$p1",Toast.LENGTH_SHORT).show()
-                    }
+                override fun onTestStarted(p0: EcgPosition) {
+                    Toast.makeText(this@LeadIITestActivity, "test started", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
-            })
-        }
+                override fun onElapsedTimeChanged(p0: Long, p1: Long) {
+                    binding.activityMainProgressbarTestStatus.progress = p0.toInt()
+                    binding.progressBar8.progress = p0.toInt()
+                }
 
-        binding.activityMainBtnShowConclusion.setOnClickListener {
-            ecgReport?.let {
-                val conclusion = (ecgReport?.conclusion as LeadTwoConclusion)
-                val characteristics = ecgReport?.ecgCharacteristics
-                binding.result.text = "$conclusion $characteristics"
+                override fun onReceivedData(p0: String) {
+
+                }
+
+                override fun onPositionRecordingComplete(
+                    p0: EcgPosition,
+                    p1: java.util.ArrayList<Double>?,
+                ) {
+                    if (p1 != null)
+                        ecgPoints[p0] = p1
+                }
+
+            }, (application as BeatoApplication).token!!)
+
+
+            binding.activityMainLayoutDeviceConnectionStatus.setBackgroundColor(if (UsbConnectionHelper.INSTANCE.isDeviceConnected) Color.GREEN else Color.RED)
+
+
+            binding.progressBar8.setOnClickListener {
+                ecgPosition = EcgPosition.LEAD_2
+                binding.activityMainTextviewCurrentPosition.text = EcgPosition.LEAD_2.name
             }
-        }
 
+            /***
+             * step :-4
+             * start ecg test.*/
+            binding.activityMainBtnStartTest.setOnClickListener {
+                if (!UsbConnectionHelper.INSTANCE.isDeviceConnected)
+                    Toast.makeText(this, "Please connect the device first.", Toast.LENGTH_SHORT)
+                        .show()
+                else if (!::ecgPosition.isInitialized)
+                    Toast.makeText(this, "please select any lead", Toast.LENGTH_SHORT).show()
+                else
+                    ecgTest.start(ecgPosition)
+            }
+
+
+            /**
+             * step :-5
+             * generate ecg report*/
+            binding.activityMainBtnGenerateReport.setOnClickListener {
+                spandanSDK.generateReport(12,
+                    ecgPoints,
+                    (application as BeatoApplication).token!!,
+                    object : OnReportGenerationStateListener {
+                        override fun onReportGenerationSuccess(p0: EcgReport) {
+                            ecgReport = p0
+                            runOnUiThread {
+                                Toast.makeText(this@LeadIITestActivity,
+                                    "report generated",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onReportGenerationFailed(p0: Int, p1: String) {
+                            Log.d("SdkImpl.TAG", "onReportGenerationFailed: $p1")
+                            runOnUiThread {
+                                Toast.makeText(this@LeadIITestActivity, "$p1", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+
+                    })
+            }
+
+            binding.activityMainBtnShowConclusion.setOnClickListener {
+                ecgReport?.let {
+                    val conclusion = (ecgReport?.conclusion as LeadTwoConclusion)
+                    val characteristics = ecgReport?.ecgCharacteristics
+                    binding.result.text = "$conclusion $characteristics"
+                }
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+        }
     }
 }
